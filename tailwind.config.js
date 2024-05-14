b0c6e1155019c48865ff7021177b143f9c65db82
Takeru Kakuta <kakuta19960915@gmail.com>
2024-05-14 22:23:29 +0900

    色を追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,tsx}"],
   theme: {
-    extend: {},
+    // 共通のカラー
+    colors: {
+      "base-color": "#56534F",
+      green: {
+        DEFAULT: "#006400",
+        2: "#228B22",
+        3: "#CCE7D3",
+      },
+    },
   },
   plugins: [],
-}
-
+};
