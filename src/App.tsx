907fd639c2b67df7ce8aa2f1c8db08b26fc8665b
Takeru Kakuta <kakuta19960915@gmail.com>
2024-05-16 14:09:40 +0900

    Feat: Header上部追加 (デスクトップ版)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/layouts/header/header";
 
 function App() {
   return (
-<>
-</>
+    <>
+      <Header />
+    </>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,19 @@ module.exports = {
     // 共通のカラー
     colors: {
       "base-color": "#56534F",
+      white: {
+        DEFAULT: "#fff",
+      },
       green: {
         DEFAULT: "#006400",
         2: "#228B22",
         3: "#CCE7D3",
+      },
+      red: {
+        DEFAULT: "#B22D35",
+      },
+      blue: {
+        DEFAULT: "#0067C0",
       },
     },
   },
