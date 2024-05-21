258a89b38481b1ebf1fca7573ca922c63d9fa808
Takeru Kakuta <kakuta19960915@gmail.com>
2024-05-21 23:56:47 +0900

    feat: ハンバーガーメニュー機能追加した #1

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import HomeIcon from "@mui/icons-material/Home";
 import InfoIcon from "@mui/icons-material/Info";
 import ApartmentIcon from "@mui/icons-material/Apartment";
@@ -8,38 +9,70 @@ import HeaderContact from "../headerContact/headerContact";
 import styles from "./nav.module.css";
 
 const Nav: React.FC = () => {
+  const [navIsOpen, setNavIsOpen] = useState(false);
+
+  const toggleNav = () => {
+    setNavIsOpen((prev) => !prev);
+  };
+
+  const closeNav = () => {
+    setNavIsOpen(false);
+  };
+
   return (
-    <div className={[styles.navContents, globalStyles.styles].join(" ")}>
-      <div className={styles.flex}>
+    <div
+      className={[
+        navIsOpen ? styles.open : styles.close,
+        globalStyles.styles,
+      ].join(" ")}
+    >
+      {navIsOpen && (
+        <style>{`
+          @media (max-width: 767px) {
+            body {
+              overflow: hidden;
+              position: fixed;
+              width: 100%;
+            }
+          }
+        `}</style>
+      )}
+
+      <button className={styles.btn} onClick={toggleNav}>
+        <span className={styles.bar}></span>
+        <span>MENU</span>
+      </button>
+
+      <div className={styles.navContents}>
         <HeaderContact />
         <nav className={styles.nav}>
           <ul>
             <li>
-              <a href="/">
+              <a href="/" onClick={closeNav}>
                 <HomeIcon />
                 ホーム
               </a>
             </li>
             <li>
-              <a href="/">
+              <a href="/" onClick={closeNav}>
                 <InfoIcon />
                 健進会について
               </a>
             </li>
             <li>
-              <a href="/">
+              <a href="/" onClick={closeNav}>
                 <ApartmentIcon />
                 施設のご案内
               </a>
             </li>
             <li>
-              <a href="/">
+              <a href="/" onClick={closeNav}>
                 <BusinessCenterIcon />
                 採用情報
               </a>
             </li>
             <li>
-              <a href="/">
+              <a href="/" onClick={closeNav}>
                 <Call />
                 お問い合わせ
               </a>
