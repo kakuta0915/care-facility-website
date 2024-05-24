36f36c91b82715c306e789ff7845fa8ea422594a
Takeru Kakuta <kakuta19960915@gmail.com>
2024-05-24 19:21:41 +0900

    ✨ Feat: モバイル版のスタイル追加 #3

--- src/home/page.tsx
+++ src/home/page.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "../components/layouts/header/header";
 import styles from "./page.module.css";
 import globalStyles from "@/src/globalStyle.module.css";
+import Footer from "../components/layouts/footer/footer";
 
 function Home() {
   return (
     <div className={globalStyles.styles}>
       <Header />
+      <Footer />
     </div>
   );
 }
