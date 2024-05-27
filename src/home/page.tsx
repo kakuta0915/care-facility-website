d0df67706bb4f80b773a0c9fc20356da845749f2
Takeru Kakuta <kakuta19960915@gmail.com>
2024-05-27 23:56:23 +0900

    Feat: contactコンポーネントを追加 #5

--- src/home/page.tsx
+++ src/home/page.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Header from "../components/layouts/header/header";
-import styles from "./page.module.css";
-import globalStyles from "@/src/globalStyle.module.css";
+import Contact from "../components/layouts/contact/contact";
 import Footer from "../components/layouts/footer/footer";
+import globalStyles from "@/src/globalStyle.module.css";
+import styles from "./page.module.css";
 
 function Home() {
   return (
     <div className={globalStyles.styles}>
       <Header />
+      <Contact />
       <Footer />
     </div>
   );
