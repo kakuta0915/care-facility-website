afab09806f56a6a15e672069ab9356e090d3d6d1
Takeru Kakuta <kakuta19960915@gmail.com>
2024-05-28 15:00:31 +0900

    ✨ Feat: Advertisementコンポーネントを作成 #7

--- src/home/page.tsx
+++ src/home/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "../components/layouts/header/header";
+import Container from "../components/layouts/container/container";
 import Contact from "../components/layouts/contact/contact";
+import Advertisement from "../components/advertisement/advertisement";
 import Footer from "../components/layouts/footer/footer";
 import globalStyles from "@/src/globalStyle.module.css";
 import styles from "./page.module.css";
@@ -9,7 +11,10 @@ function Home() {
   return (
     <div className={globalStyles.styles}>
       <Header />
-      <Contact />
+      <Container>
+        <Contact />
+        <Advertisement />
+      </Container>
       <Footer />
     </div>
   );
